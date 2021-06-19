ee2255ccf0f78c82193c9c5e1a50eef2b76994d5
massytak <massi.messalti@gmail.com>
2021-06-19 14:50:39 +0200

    iteration 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import './App.css'
-import 'bulma/css/bulma.css';
-import NavBar from './navbar/Navbar';
-import FormField from './formfield/FormField';
+import React from "react";
+import "./App.css";
+import "bulma/css/bulma.css";
+import NavBar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
+import CoolButton from "./button/Button";
 
 const App = () => {
   return (
     <>
       <NavBar />;
       <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
-      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      <CoolButton isSmall isDanger className="is-rounded my-class">
+        Button 1
+      </CoolButton>
+      <CoolButton isSmall isSuccess>
+        Button 2
+      </CoolButton>
     </>
   );
 };
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,46 +1,49 @@
-import React from 'react';
-import 'bulma/css/bulma.css';
+import React from "react";
+import "bulma/css/bulma.css";
 
-
-function NavBar(){
+function NavBar() {
   return (
-<nav className="navbar" role="navigation" aria-label="main navigation">
-  <div className="navbar-brand">
-    <a className="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
-    </a>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            height="28"
+          />
+        </a>
 
-    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" className="navbar-menu">
-    <div className="navbar-start">
-      <a className="navbar-item">
-        Home
-      </a>
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-    </div>
 
-    <div className="navbar-end">
-      <div className="navbar-item">
-        <div className="buttons">
-          <a className="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a className="button is-light">
-            Log in
-          </a>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">Home</a>
         </div>
       </div>
-    </div>
-</nav>
 
-  )
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <div className="buttons">
+            <a className="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a className="button is-light">Log in</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
-
