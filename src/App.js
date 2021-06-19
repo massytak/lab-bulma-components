a3e38a23ce2d294b10b20ef5413d1345385531b4
lotiko <l.despres@netc.fr>
2021-06-19 14:19:31 +0200

    itération 2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './App.css'
 import 'bulma/css/bulma.css';
 import NavBar from './navbar/Navbar';
+import FormField from './formfield/FormField';
 
 const App = () => {
-  return <NavBar/>;
+  return (
+    <>
+      <NavBar />;
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+    </>
+  );
 };
 
 export default App;
